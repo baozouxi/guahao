abc1f810d35fe9022c93a359d25e3eea892fc023
baozouxi <253120625@qq.com>
2017-09-01 17:13:08 +0800

    20170901

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,38 +10,43 @@ class LoginController extends Controller
 {
     public function index(Request $req)
     {
-    	$this->validate($req, [
-    		'username' => 'required|string|min:5',
-    		'password' => 'required|string|min:5',
-    		'code' => 'nullable|numeric',
-    		]);
-    	$data = $req->all();
+        $this->validate($req, [
+            'username' => 'required|string|min:5',
+            'password' => 'required|string|min:5',
+            'code'     => 'nullable|numeric',
+        ]);
+        $data = $req->all();
 
-    	$error = ['code'=>'1', 'msg'=>'用户名，密码错误', 'time'=>getNow()];
+        $error = ['code' => '1', 'msg' => '用户名，密码错误', 'time' => getNow()];
 
-    	if(!$user = User::where('username', $data['username'])->first()) return  $error;
+        if ( ! $user = User::where('username', $data['username'])->first()) {
+            return $error;
+        }
 
-    	if($user->password !== sha1($data['password'])) return $error;
+        if ($user->password !== sha1($data['password'])) {
+            return $error;
+        }
 
-    	$role = Role::find($user->role_id)->toArray();
+        $role = Role::find($user->role_id)->toArray();
 
-    	$acc_nodes = unserialize($role['nodes']);	
+        $acc_nodes = unserialize($role['nodes']);
 
-    	$req->session()->put('user_id', $user->id);
+        $req->session()->put('user_id', $user->id);
 
         $req->session()->put('role_id', $user->role_id);
-    	
-    	$req->session()->put('name', $user->name);
 
-    	$req->session()->put('access_nodes', $acc_nodes);
+        $req->session()->put('name', $user->name);
 
-    	return ['code'=>'0', 'msg'=>route('index'), 'time'=>getNow()];
-    }	
+        $req->session()->put('access_nodes', $acc_nodes);
+
+        return ['code' => '0', 'msg' => route('index'), 'time' => getNow()];
+    }
 
 
     public function logout(Request $req)
     {
         $req->session()->flush();
+
         return redirect('/login');
     }
 }
--- app/Http/Controllers/NavController.php
+++ app/Http/Controllers/NavController.php
@@ -10,12 +10,12 @@ class NavController extends Controller
 {
     public function index()
     {
-        $nav_arr = Nav::orderBy('sort', 'desc')->get()->toArray();
+        $nav_arr    = Nav::orderBy('sort', 'desc')->get()->toArray();
         $nav_reduce = [];
 
         foreach ($nav_arr as $key => $navItem) {
-            if($navItem['parent_id'] == '0') {
-                $navItem['nav_child'] = [];
+            if ($navItem['parent_id'] == '0') {
+                $navItem['nav_child']       = [];
                 $nav_reduce[$navItem['id']] = $navItem;
                 unset($nav_arr[$key]);
             }
@@ -27,55 +27,74 @@ class NavController extends Controller
                 unset($nav_arr[$key]);
             }
         }
-        
 
-        return view('nav.index', ['nav'=> $nav_reduce]);
+
+        return view('nav.index', ['nav' => $nav_reduce]);
     }
 
     public function create()
     {
         $nav_top_arr = Nav::where('parent_id', '0')->get();
-    	return view('Nav.create', ['nav_top_arr'=>$nav_top_arr]);
+
+        return view('Nav.create', ['nav_top_arr' => $nav_top_arr]);
     }
 
     public function store(NavRequest $req)
     {
-    	$data = $req->all();
-        if(Nav::create($data))  return code(route('nav.index'), '-4');
+        $data = $req->all();
+        if (Nav::create($data)) {
+            return code(route('nav.index'), '-4');
+        }
+
         return code('添加失败，请重试', '1');
     }
 
 
     public function edit($id)
     {
-        if(!$nav = Nav::find($id)) return code('非法操作，请刷新', '1');
-        $topNav = Nav::whereNotIn('id',[$id])->get();
-        return view('Nav.edit', ['nav'=>$nav,'nav_top_arr'=>$topNav]);
+        if ( ! $nav = Nav::find($id)) {
+            return code('非法操作，请刷新', '1');
+        }
+        $topNav = Nav::whereNotIn('id', [$id])->get();
+
+        return view('Nav.edit', ['nav' => $nav, 'nav_top_arr' => $topNav]);
     }
 
     public function update(NavRequest $req, $id)
     {
 
         $data = $req->all();
-        if(!$nav = Nav::find($id)) return code('非法操作，请刷新', '1');
-        if ($data['parent_id'] != $nav->parent_id) {
-            if(Nav::where('parent_id', $id)->get()->toArray()) return code('该栏目下有子栏目，不支持移动', '1');
+        if ( ! $nav = Nav::find($id)) {
+            return code('非法操作，请刷新', '1');
         }
-        $nav->name = $data['name'];
-        $nav->url = $data['url'];
-        $nav->icon = $data['icon'];
+        if ($data['parent_id'] != $nav->parent_id) {
+            if (Nav::where('parent_id', $id)->get()->toArray()) {
+                return code('该栏目下有子栏目，不支持移动', '1');
+            }
+        }
+        $nav->name      = $data['name'];
+        $nav->url       = $data['url'];
+        $nav->icon      = $data['icon'];
         $nav->parent_id = $data['parent_id'];
-        if ($nav->save()) return code('修改成功', '-4');
+        if ($nav->save()) {
+            return code('修改成功', '-4');
+        }
+
         return code('修改失败，请刷新重试', '1');
 
     }
 
-    public function sort($id, $sort='0')
+    public function sort($id, $sort = '0')
     {
-        if (!$nav = Nav::find($id)) return code('非法操作，请刷新', '1');
+        if ( ! $nav = Nav::find($id)) {
+            return code('非法操作，请刷新', '1');
+        }
         $nav->sort = (int)$sort;
-        if($nav->save()) return code('<i>'.(int)$sort.'<i>', '1');
+        if ($nav->save()) {
+            return code('<i>' . (int)$sort . '<i>', '1');
+        }
+
         return code('修改失败', '-1');
     }
-    
+
 }
